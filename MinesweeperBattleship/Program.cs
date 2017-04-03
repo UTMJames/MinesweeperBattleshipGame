b4657959da96782fac5d16a06bfb9eb04c67c373
James Smith <jamesmit@ut.utm.edu>
2017-04-03 15:48:01 -0500

    Start menu wpf

--- MinesweeperBattleship/Program.cs
+++ MinesweeperBattleship/Program.cs
@@ -10,6 +10,17 @@ namespace MinesweeperBattleship
     {
         static void Main(string[] args)
         {
+            char[,] gameboard = new char[10, 10];
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write("|");
+                for(int j = 0; j < 10; j++)
+                {
+                    gameboard[i, j] = ' ';
+                    Console.WriteLine("_");
+                }
+            }
+            Console.Read();
         }
     }
 }
